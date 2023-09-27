94c81cd0847d00e973b7859043f9b921cbaae1e3
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-09-27 15:05:57 -0700

    :=

--- tutorial.go
+++ tutorial.go
@@ -4,4 +4,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to my nightmare!")
+
+	// var name string = "Cheryl"
+	// The below syntax gets Go to guess the type.
+	name := "Cheryl"
+	// if it's a float a number must be assigned to it to show how much computer memory is being used ex float64
+	// if a variable isn't used go will give you an error
+	fmt.Println(name)
+
 }
