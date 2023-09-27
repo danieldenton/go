94370698e6c7675b84c18a991e57519fdc19e472
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-09-27 16:06:52 -0700

    declare multiple variables

--- tutorial.go
+++ tutorial.go
@@ -47,5 +47,6 @@ func main() {
 	var answer2 string
 	fmt.Scan(&answer, &answer2)
 	// if there was a space in the answer the answer variable decleration would be over.
+	// if there's 2 variables it's gonna wait for 2 variables.
 	fmt.Println(answer, answer2)
 }
