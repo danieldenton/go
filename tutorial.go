7cb9f7e2fc787b04e19979486d4d8072a442beb4
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-09-27 15:14:26 -0700

    string formatting links added to comments

--- tutorial.go
+++ tutorial.go
@@ -8,8 +8,13 @@ func main() {
 	// var name string = "Cheryl"
 	// The below syntax gets Go to guess the type.
 	name := "Cheryl"
+	age := 40
 	// if it's a float a number must be assigned to it to show how much computer memory is being used ex float64
 	// if a variable isn't used go will give you an error
-	fmt.Println(name)
 
+	// notice Printf is differnt than Println (print line)
+	// %v is a place holder
+	// fmt.Printf("Hello %v %v, how are you doing???", name, name)
+	fmt.Printf("Hello %v, You are %v", name, age)
+	// https://gobyexample.com/string-formatting
 }
